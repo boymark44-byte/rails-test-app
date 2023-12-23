4870274f400c0615782aa30f40dfe22f6778b4a8
Mark Glenn <markglenn.gabuya@g.msuiit.edu.ph>
2023-12-23 13:47:05 +0800

    Initial commit and add root route

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+  # Actions or Methods define, both are correct
+  # Generates an HTML template
+  # def hello
+  #   render html: 'Hello World!'
+  # end
+end
